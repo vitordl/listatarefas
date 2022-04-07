b8536346c72688e4b688b6a35bc3a536205d8fb6
vitordl <48863970+vitordl@users.noreply.github.com>
2022-04-07 17:21:10 -0300

    API inicio adicionada em listatarefas

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    public function index(){
+        return response()->json($this->moviesAll());
+    }
+
+    public function getMovie($id){
+       //$movies = $this->moviesAll();
+       $movies =  null;
+       foreach($this->moviesAll() as $mov){
+           if($mov['id'] == $id){
+               $movies = $mov;
+           }
+       }
+       return $movies ? response()->json($movies) : "erro";
+    }
+
+    public function getMovieByGenre($genre){
+        $movies = null;
+        foreach($this->moviesAll() as $mov){
+            if($mov['genre'] == $genre){
+                $movies = $mov;
+            }
+
+        }
+        return $movies ? response()->json($movies) : 'erro genre';
+    }
+
+    public function store(Request $request){
+        $validar = $request->validate(
+            [
+                'id' => 'numeric',
+                'name' => 'required'
+            ]
+
+        );
+        return response()->json($request->all());
+    }
+
+    public function update(){
+        return "O item foi atualizado";
+    }
+
+    public function delete(){
+        return "Foi removido com sucesso";
+    }
+
+
+    protected function moviesAll(){
+        return [
+            [
+            'name' => 'A lagoa azul',
+            'id' => 1,
+            'genre' => 'Duvidoso',
+            ],
+
+            [
+            'name' => 'Spot',
+            'id' => 2,
+            'genre' => 'Comédia',
+            ],
+
+            [
+            'name' => 'O Chamado',
+            'id' => 3,
+            'genre' => 'Suspense',
+            ]
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('movies', 'MoviesController@index');
+Route::get('movies/{id}', 'MoviesController@getMovie');
+Route::get('movies_genero/{genre}', 'MoviesController@getMovieByGenre');
+Route::post('movies/store', 'MoviesController@store');
+Route::put('movies/update', 'MoviesController@update');
+Route::delete('movies/delete', 'MoviesController@delete');
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
